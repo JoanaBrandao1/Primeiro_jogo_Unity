bbc0fb09e89c74de13b59f511f350f5d7e8864ec
JoanaBrandao01 <helson.santos25@gmail.com>
2025-01-02 21:15:40 -0300

    Movimento do player

---
+++ Assets/script/MovimentoDoPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+public class MovimentoDoPlayer : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private Vector2 moveVelocity;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+    void FixedUpdate(){
+        rb.MovePosition(rb. position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
